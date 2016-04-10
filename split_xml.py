a3071a2aca6dc9aa97f88baf71433a7bfdf0f31b
Ginsea Chen <ginseachen@hotmail.com>
2016-04-10 19:32:41 +0800

    Create split_xml.py

---
+++ split_xml.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+'''
+@Description This script can be used to split large xml file of blast
+@Author This script is developed by Ginsea Chen (ginseachen@hotmail.com) in CATAS
+@Usage python split_xml.py --xml blast.xml --num [NUM]
+'''
+import os
+import argparse
+import os.path
+
+def parse_args():
+    parser = argparse.ArgumentParser(usage="%(prog)s[options]")
+    parser.add_argument("--xml",default="blast.xml",help="Your blast output file with xml format (outfmt 5)")
+    parser.add_argument("--num",default=1000, type=int,help="how many seqs in a split file")
+    return parser.parse_args()
+
+def split_xml(xml,num):
+    fp = open(xml)
+    inf = fp.read()
+    ml = inf.split("<Iteration>")
+    y = len(ml) / int(num)
+    os.system("mkdir split")
+    i = 1
+    while i <= y:
+        if i < y:
+            out = open("split/%s.xml"%i,"w")
+            out.write("split/%s"%ml[0].rstrip())
+            for x in range(int(num)*(i-1),int(num)*i):
+                out.write("\n<Iteration>\n%s"%ml[x+1].strip())
+            out.write("\n</BlastOutput_iterations>\n")
+            out.write("</BlastOutput>")
+        elif i == y:
+            out = open("%s.xml"%i,"w")
+            out.write("test/%s\n"%ml[0].rstrip())
+            for x in range(int(num)*(i - 1),len(ml) - 1):
+                out.write("\n<Iteration>\n%s\n"%ml[x+1].strip())
+        i += 1
+
+def main():
+    argser = parse_args()
+    split_xml(argser.xml,int(argser.num))
+
+if __name__ == "__main__":
+    main()
